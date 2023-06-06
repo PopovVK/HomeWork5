e15882372bd00b42076f7203b53482eb1e04cdf3
PopovVK <vladsmartvladsmart@gmail.com>
2023-06-06 18:53:42 +0300

    final

--- index.js
+++ index.js
@@ -1,49 +1,11 @@
-const numbers = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
-
-let summaPositive = 0;
-let countPositive = 0;
-let maxElement = numbers[0];
-let maxElementIndex = 0;
-let countNegative = 0;
-let countOddPositive = 0;
-let countEvenPositive = 0;
-let summaEvenPositive = 0;
-let summaOddPositive = 0;
-let productPositive = 1;
+const numbers = [5, 8, -1, '-6', 11, 'ololo', '-2'];
+let sum = 0;
 
 for (let i = 0; i < numbers.length; i++) {
-  const currentNumber = numbers[i];
-
-  if (currentNumber > 0) {
-    summaPositive += currentNumber;
-    countPositive++;
-    productPositive *= currentNumber;
-
-    if (currentNumber % 2 === 0) {
-      countEvenPositive++;
-      summaEvenPositive += currentNumber;
-    } else {
-      countOddPositive++;
-      summaOddPositive += currentNumber;
-    }
-  } else {
-    countNegative++;
-  }
-
-  if (currentNumber > maxElement) {
-    maxElement = currentNumber;
-    maxElementIndex = i;
+  const parsedNumber = parseInt(numbers[i], 10);
+  if (!isNaN(parsedNumber)) {
+    sum += parsedNumber;
   }
 }
 
-
-alert("Сума позитивних елементів: " + summaPositive + "\n" +
-  "Кількість позитивних елементів: " + countPositive + "\n" +
-  "Максимальний елемент: " + maxElement + "\n" +
-  "Порядковий номер максимального елементу: " + (maxElementIndex ) + "\n" +
-  "Кількість негативних елементів: " + countNegative + "\n" +
-  "Кількість непарних позитивних елементів: " + countOddPositive + "\n" +
-  "Кількість парних позитивних елементів: " + countEvenPositive + "\n" +
-  "Сума парних позитивних елементів: " + summaEvenPositive + "\n" +
-  "Сума непарних позитивних елементів: " + summaOddPositive + "\n" +
-  "Добуток позитивних елементів: " + productPositive)
+alert('Sum is ' + sum);
